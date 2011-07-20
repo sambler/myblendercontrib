a7d265e570c217c7c249408417088abaaf5a186a
funkywyrm <funkywyrm@96b784f8-4ae3-dd11-aff7-0030488c597c>
2011-07-20 17:14:54 +0000

    Fix for incorrect function name.
    git-svn-id: https://svn.blender.org/svnroot/bf-extensions/contrib/py/scripts/addons@2148 96b784f8-4ae3-dd11-aff7-0030488c597c

--- game_engine_ragdolls_kit/brik.py
+++ game_engine_ragdolls_kit/brik.py
@@ -911,7 +911,7 @@ class brik_create_hit_boxes(bpy.types.Operator):
             for bone in bones:
                 if bone.bone.use_deform:
                     #Create boxes that do not exist
-                    hit_box,volume = create_shape_box(self.hit_box_prefix, armature, bone)
+                    hit_box,volume = create_box_envelope(self.hit_box_prefix, armature, bone)
                     #hit_box = create_box(self.hit_box_prefix, self.hit_box_length, self.hit_box_width, armature, bone)
                     armature['brik_bone_hit_box_dict'][bone.name] = hit_box.name
                 
