6f50ca21337ea33d63e8874b2209457429a4d69f
meta-androcto <meta-androcto@96b784f8-4ae3-dd11-aff7-0030488c597c>
2011-08-07 10:02:14 +0000

    fix for reverse vector multiplication updated working revision number
    git-svn-id: https://svn.blender.org/svnroot/bf-extensions/contrib/py/scripts/addons@2223 96b784f8-4ae3-dd11-aff7-0030488c597c

--- mesh_bevel/__init__.py
+++ mesh_bevel/__init__.py
@@ -25,7 +25,7 @@ bl_info = {
     'author': 'chromoly',
     'version': (0, 4),
     'blender': (2, 5, 7),
-    'api': 36505,
+    'api': 39104,
     'location': 'View3D > EditMode > Specials',
     'url': '',
     'category': 'Mesh'}
@@ -229,7 +229,7 @@ def bevel(ob, follow, beveltype):
                 vr010c_cross = vr01.cross(vr0c).normalized()
                 angle = vr01.angle(vr02)
                 q = axis_angle_to_quat(vr010c_cross, angle / 2)
-                v = vr01 * q
+                v = q * vr01 
                 v.normalize()
                 if angle > SMALL_NUMBER:
                     s = math.sin(angle / 2)
