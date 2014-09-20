22f505d5a5568209ee37b69dee2fd0db2a2b546c
sambler <Develop@Shaneware.Biz>
2014-09-20 20:51:29 +0930

    update scripts by Sebastian Koenig

---
+++ snapping_pies.py
@@ -0,0 +1,219 @@
+bl_info = {
+    "name": "Snapping Pies",
+    "author": "Sebastian Koenig",
+    "version": (0, 1),
+    "blender": (2, 71, 6),
+    "description": "Custom Pie Menus",
+    "category": "3D View",}
+
+
+
+import bpy
+from bpy.types import Menu
+
+
+###### FUNTIONS ##########
+
+def origin_to_selection(context):
+    context = bpy.context
+
+    if context.object.mode == "EDIT":
+        saved_location = context.scene.cursor_location.copy()
+        bpy.ops.view3d.snap_cursor_to_selected()
+        bpy.ops.object.mode_set(mode="OBJECT")
+        bpy.ops.object.origin_set(type='ORIGIN_CURSOR')
+        context.scene.cursor_location = saved_location
+
+
+def origin_to_geometry(context):
+    context = bpy.context
+
+    if context.object.mode == "EDIT":
+        bpy.ops.object.mode_set(mode="OBJECT")
+        bpy.ops.object.origin_set(type='ORIGIN_GEOMETRY')
+        bpy.ops.object.mode_set(mode="EDIT")
+
+
+########### CUSTOM OPERATORS ###############
+
+class VIEW3D_OT_origin_to_selected(bpy.types.Operator):
+    bl_idname="object.origin_to_selected"
+    bl_label="Origin to Selection"
+
+    @classmethod
+    def poll(cls, context):
+        sc = context.space_data
+        return (sc.type == 'VIEW_3D')
+
+    def execute(self, context):
+        origin_to_selection(context)
+        return {'FINISHED'}
+
+
+class VIEW3D_OT_origin_to_geometry(bpy.types.Operator):
+    bl_idname="object.origin_to_geometry"
+    bl_label="Origin to Geometry"
+
+    @classmethod
+    def poll(cls, context):
+        sc = context.space_data
+        return (sc.type == 'VIEW_3D')
+
+    def execute(self, context):
+        origin_to_geometry(context)
+        return {'FINISHED'}
+
+
+#Menu Snap Target
+class VIEW3D_OT_SnapTargetMenu(Menu):
+    bl_idname = "snap.targetmenu"
+    bl_label = "Snap Target Menu"
+
+    def draw(self, context):
+        layout = self.layout
+
+        layout.operator("object.snaptargetvariable", text="Active").variable='ACTIVE'
+        layout.operator("object.snaptargetvariable", text="Median").variable='MEDIAN'
+        layout.operator("object.snaptargetvariable", text="Center").variable='CENTER'
+        layout.operator("object.snaptargetvariable", text="Closest").variable='CLOSEST'
+
+
+
+class VIEW3D_OT_SnapTargetVariable(bpy.types.Operator):
+    bl_idname = "object.snaptargetvariable"
+    bl_label = "Snap Target Variable"
+    variable = bpy.props.StringProperty()
+
+    @classmethod
+    def poll(cls, context):
+        return True
+
+    def execute(self, context):
+        bpy.context.scene.tool_settings.snap_target=self.variable
+        return {'FINISHED'}
+
+
+#Menu Snap Element
+class VIEW3D_OT_SnapElementMenu(Menu):
+    bl_idname = "snap.snapelementmenu"
+    bl_label = "Snap Element"
+    settings = bpy.context.scene.tool_settings
+
+    def draw(self, context):
+        layout = self.layout
+
+        layout.operator("object.snapelementvariable", text="Vertex").variable='VERTEX'
+        layout.operator("object.snapelementvariable", text="Edge").variable='EDGE'
+        layout.operator("object.snapelementvariable", text="Face").variable='FACE'
+        layout.operator("object.snapelementvariable", text="Increment").variable='INCREMENT'
+        layout.operator("object.snapelementvariable", text="Volume").variable='VOLUME'
+
+
+class VIEW3D_OT_SnapElementVariable(bpy.types.Operator):
+    bl_idname = "object.snapelementvariable"
+    bl_label = "Snap Element Variable"
+    variable = bpy.props.StringProperty()
+
+    @classmethod
+    def poll(cls, context):
+        return True
+
+    def execute(self, context):
+        bpy.context.scene.tool_settings.snap_element=self.variable
+        return {'FINISHED'}
+
+
+################### PIES #####################
+
+class VIEW3D_PIE_origin(Menu):
+    # label is displayed at the center of the pie menu.
+    bl_label = "Origin"
+    bl_idname = "object.snapping_pie"
+
+    def draw(self, context):
+        context = bpy.context
+        layout = self.layout
+        tool_settings = context.scene.tool_settings
+
+        pie = layout.menu_pie()
+
+        pie.operator("view3d.snap_selected_to_cursor", icon="CURSOR")
+        pie.operator("view3d.snap_cursor_to_selected", icon="CLIPUV_HLT")
+
+        # set origin to selection in Edit Mode, set origin to cursor in Object mode
+        if context.object.mode == "EDIT":
+            pie.operator("object.origin_to_selected", icon="OUTLINER_OB_EMPTY")
+        else:
+            pie.operator("object.origin_set",icon="EMPTY_DATA", text="Origin to Cursor").type="ORIGIN_CURSOR"
+
+
+        #op = pie.operator("wm.context_set_enum", text="Edge Snapping", icon="SNAP_EDGE")
+        #op.data_path="tool_settings.snap_element"
+        #op.value="EDGE"
+
+
+        if context.object.mode == "OBJECT":
+            pie.operator("object.origin_set",icon="MESH_CUBE", text="Origin to Geometry").type="ORIGIN_GEOMETRY"
+        else:
+            pie.operator("object.origin_to_geometry", icon="MESH_CUBE")
+
+        pie.operator("view3d.snap_cursor_to_center", icon="CURSOR")
+
+
+        if tool_settings.snap_element=="VERTEX":
+            pie.menu("snap.snapelementmenu", text="Snap Element", icon="SNAP_VERTEX")
+        elif tool_settings.snap_element=="EDGE":
+            pie.menu("snap.snapelementmenu", text="Snap Element", icon="SNAP_EDGE")
+        elif tool_settings.snap_element=="FACE":
+            pie.menu("snap.snapelementmenu", text="Snap Element", icon="SNAP_FACE")
+        elif tool_settings.snap_element=="VOLUME":
+            pie.menu("snap.snapelementmenu", text="Snap Element", icon="SNAP_VOLUME")
+        elif tool_settings.snap_element=="INCREMENT":
+            pie.menu("snap.snapelementmenu", text="Snap Element", icon="SNAP_INCREMENT")
+
+        if tool_settings.use_snap:
+            pie.prop(context.scene.tool_settings, "use_snap", text="Use Snap(ON)")
+        else:
+            pie.prop(context.scene.tool_settings, "use_snap", text="Use Snap(OFF)")
+
+        pie.menu("snap.targetmenu", text="Snap Target", icon='SNAP_SURFACE')
+
+
+
+########## REGISTER ############
+
+def register():
+    bpy.utils.register_class(VIEW3D_PIE_origin)
+    bpy.utils.register_class(VIEW3D_OT_origin_to_selected)
+    bpy.utils.register_class(VIEW3D_OT_origin_to_geometry)
+    bpy.utils.register_class(VIEW3D_OT_SnapTargetMenu)
+    bpy.utils.register_class(VIEW3D_OT_SnapTargetVariable)
+    bpy.utils.register_class(VIEW3D_OT_SnapElementVariable)
+    bpy.utils.register_class(VIEW3D_OT_SnapElementMenu)
+
+
+    wm = bpy.context.window_manager
+
+    km = wm.keyconfigs.addon.keymaps.new(name = 'Object Mode')
+    kmi = km.keymap_items.new('wm.call_menu_pie', 'S', 'PRESS', shift=True).properties.name = "object.snapping_pie"
+
+    km = wm.keyconfigs.addon.keymaps.new(name = 'Mesh')
+    kmi = km.keymap_items.new('wm.call_menu_pie', 'S', 'PRESS',shift=True).properties.name = "object.snapping_pie"
+
+
+
+
+def unregister():
+
+    bpy.utils.unregister_class(VIEW3D_PIE_origin)
+    bpy.utils.unregister_class(VIEW3D_OT_origin_to_selected)
+    bpy.utils.unregister_class(VIEW3D_OT_origin_to_geometry)
+    bpy.utils.unregister_class(VIEW3D_OT_SnapTargetMenu)
+    bpy.utils.unregister_class(VIEW3D_OT_SnapTargetVariable)
+    bpy.utils.unregister_class(VIEW3D_OT_SnapElementVariable)
+    bpy.utils.unregister_class(VIEW3D_OT_SnapElementMenu)
+
+
+if __name__ == "__main__":
+    register()
+    #bpy.ops.wm.call_menu_pie(name="mesh.mesh_operators")
--- tracking_pies.py
+++ tracking_pies.py
@@ -1,48 +1,71 @@
+bl_info = {
+"name": "Tracking Pies",
+"author": "Sebastian Koenig",
+"version": (1, 0),
+"blender": (2, 7, 2),
+"location": "Clip Editor > Masking Pies",
+"description": "Pie Controls for Tracking",
+"warning": "",
+"wiki_url": "",
+"tracker_url": "",
+"category": "Movie Tracking"}
+
+'''
+This script adds tracking pies to the clip editor.
+Hotkeys:
+Q:      Marker Setup
+W:      Setup Clip and Display options
+E:      Tracking commands
+Shift+S: Solving  
+Shift+W: Scene Reconstruction
+OSkey+A: Playback and frame jumping commands
+'''
+
+
 import bpy
 from bpy.types import Menu
 
 
-############### FUNCTIONS ##########################
-
-
-
 
 class CLIP_marker_pie(Menu):
-    # label is displayed at the center of the pie menu.
+    # Settings for the individual markers
     bl_label = "Marker Settings"
     bl_idname = "clip.marker_pie"
 
     def draw(self, context):
 
-
         layout = self.layout
         clip = context.space_data.clip
         settings = clip.tracking.settings
+        active = clip.tracking.tracks.active
 
         pie = layout.menu_pie()
-        active = clip.tracking.tracks.active
 
         op = pie.operator("wm.context_set_enum", text="Loc", icon="OUTLINER_DATA_EMPTY")
         op.data_path="space_data.clip.tracking.tracks.active.motion_model"
         op.value="Loc"
-        
         op = pie.operator("wm.context_set_enum", text="Affine", icon="OUTLINER_DATA_LATTICE")
         op.data_path="space_data.clip.tracking.tracks.active.motion_model"
         op.value="Affine"
 
-
-
-        pie.prop(active, "use_blue_channel", text="Blue Channel", icon="FILTER")
-     
+        if clip.tracking.tracks.active.use_blue_channel:
+            pie.prop(active, "use_blue_channel", text="Blue Channel (ON)", icon="FILTER")
+        else:
+            pie.prop(active, "use_blue_channel", text="Blue Channel (OFF)", icon="FILTER")
         pie.operator("clip.track_settings_as_default", icon="SETTINGS")
 
-        pie.prop(active, "use_normalization", text="Normalization", icon="IMAGE_ALPHA")
-        pie.prop(active, "use_brute", text="Prepass", icon="FILTER")
+        if clip.tracking.tracks.active.use_normalization:
+            pie.prop(active, "use_normalization", text="Normalization (ON)", icon="IMAGE_ALPHA")
+        else:
+            pie.prop(active, "use_normalization", text="Normalization (OFF", icon="IMAGE_ALPHA")
+        if clip.tracking.tracks.active.use_brute:
+            pie.prop(active, "use_brute", text="Prepass (ON)", icon="FILTER")
+        else:
+            pie.prop(active, "use_brute", text="Prepass (OFF)", icon="FILTER")
 
         op = pie.operator("wm.context_set_enum", text="Match Previous", icon="KEY_HLT")
         op.data_path="space_data.clip.tracking.tracks.active.pattern_match"
         op.value="PREV_FRAME"
-
         op = pie.operator("wm.context_set_enum", text="Match Keyframe", icon="KEY_HLT")
         op.data_path="space_data.clip.tracking.tracks.active.pattern_match"
         op.value="KEYFRAME"
@@ -52,14 +75,15 @@ class CLIP_marker_pie(Menu):
 
 
 class CLIP_tracking_pie(Menu):
-    # label is displayed at the center of the pie menu.
-    bl_label = "Tracking Settings"
+    # Tracking Operators
+    bl_label = "Tracking"
     bl_idname = "clip.tracking_pie"
 
     def draw(self, context):
         layout = self.layout
 
         pie = layout.menu_pie()
+        
         prop = pie.operator("clip.track_markers", icon="PLAY_REVERSE")
         prop.backwards = True
         prop.sequence = True
@@ -67,52 +91,131 @@ class CLIP_tracking_pie(Menu):
         prop.backwards = False
         prop.sequence = True
         
-        pie.operator("clip.detect_features")
+        pie.operator("clip.detect_features", icon="ZOOM_SELECTED")
         pie.operator("clip.disable_markers", icon="RESTRICT_VIEW_ON")
+
         pie.operator("clip.clear_track_path", icon="BACK").action="UPTO"
         pie.operator("clip.clear_track_path", icon="FORWARD").action="REMAINED"
+
         pie.operator("clip.refine_markers", icon="LOOP_BACK").backwards=True
         pie.operator("clip.refine_markers", icon="LOOP_FORWARDS").backwards=False
 
 
+
 class CLIP_clipsetup_pie(Menu):
-    # label is displayed at the center of the pie menu.
-    bl_label = "Clip Editor Pie"
+    # Setup the clip display options
+    bl_label = "Clip and Display Setup"
     bl_idname = "clip.clipsetup_pie"
 
     def draw(self, context):
         layout = self.layout
+        clip = context.space_data.clip
+        sc = context.space_data
 
         pie = layout.menu_pie()
+
         pie.operator("clip.reload", text="Reload Footage", icon="FILE_REFRESH")
         pie.operator("clip.prefetch", text="Prefetch Footage", icon="LOOP_FORWARDS")
-        pie.prop(context.space_data, "use_mute_footage", text="Mute Footage", icon="MUTE_IPO_ON")
-        pie.prop(context.space_data.clip_user, "use_render_undistorted", text="Render Undistorted", icon="MESH_GRID")
-        pie.prop(context.space_data, "show_names", text="Show Track Info", icon="WORDWRAP_ON")
-        pie.prop(context.space_data, "lock_selection", text="Lock", icon="LOCKED")
-        pie.prop(context.space_data, "show_disabled", text="Show Disabled Tracks", icon="VISIBLE_IPO_ON")
-        pie.prop(context.space_data, "show_marker_search", text="Search Area", icon="EDIT_VEC")
+        
+        pie.prop(sc, "use_mute_footage", text="Mute Footage", icon="MUTE_IPO_ON")
+        if sc.clip_user.use_render_undistorted:
+            pie.prop(sc.clip_user, "use_render_undistorted", text="Render Undistorted (ON)", icon="MESH_GRID")
+        else:
+            pie.prop(sc.clip_user, "use_render_undistorted", text="Render Undistorted (OFF)", icon="MESH_GRID")
+
+        pie.prop(sc, "show_names", text="Show Track Info", icon="WORDWRAP_ON")
+        pie.prop(sc, "lock_selection", text="Lock", icon="LOCKED")
+        
+        pie.prop(sc, "show_disabled", text="Show Disabled Tracks", icon="VISIBLE_IPO_ON")
+        pie.prop(sc, "show_marker_search", text="Display Search Area", icon="EDIT_VEC")
+
+
+
+
+class CLIP_solver_pie(Menu):
+    # Operators to solve the scene
+    bl_label = "Solving"
+    bl_idname = "clip.solver_pie"
+
+    def draw(self, context):
+        layout = self.layout
+        sc = context.space_data
+        clip = context.space_data.clip
+        settings = clip.tracking.settings
+
+        pie = layout.menu_pie()
+
+        pie.operator("clip.create_plane_track", icon="MESH_PLANE")
+        pie.operator("clip.solve_camera", text="Solve Camera", icon="OUTLINER_OB_CAMERA")
+
+        op= pie.operator("clip.clean_tracks", icon="STICKY_UVS_DISABLE")
+        op.frames=15
+        op.error=2
+        if settings.use_tripod_solver:
+            pie.prop(settings, "use_tripod_solver", text="Tripod Solver (ON)", icon="RESTRICT_RENDER_OFF")
+        else:
+            pie.prop(settings, "use_tripod_solver", text="Tripod Solver (OFF)", icon="RESTRICT_RENDER_ON")
+
+
+        pie.operator("clip.set_solver_keyframe", text="Set Keyframe A", icon="KEY_HLT").keyframe='KEYFRAME_A'
+        pie.operator("clip.set_solver_keyframe", text="Set Keyframe B", icon="KEY_HLT").keyframe='KEYFRAME_B'
+
+        op = pie.operator("wm.context_set_enum", text="No refinement", icon="CAMERA_DATA")
+        op.data_path="space_data.clip.tracking.settings.refine_intrinsics"
+        op.value="NONE"
+        op = pie.operator("wm.context_set_enum", text="Refine Focal Length", icon="CAMERA_DATA")
+        op.data_path="space_data.clip.tracking.settings.refine_intrinsics"
+        op.value="FOCAL_LENGTH"
 
 
 
 
 class CLIP_reconstruction_pie(Menu):
     # label is displayed at the center of the pie menu.
-    bl_label = "Scene Reconstruction"
+    bl_label = "Reconstruction"
     bl_idname = "clip.reconstruction_pie"
+
+
+    def draw(self, context):
+
+        clip = context.space_data.clip
+        settings = clip.tracking.settings
+        layout = self.layout
+
+        pie = layout.menu_pie()
+  
+        pie.operator("clip.set_scale", text="Set Scale", icon="ARROW_LEFTRIGHT")
+        pie.operator("clip.setup_tracking_scene", text="Setup Tracking Scene", icon="SCENE_DATA")
+  
+        pie.operator("clip.set_plane", text="Setup Floor", icon="MESH_PLANE")
+        pie.operator("clip.set_origin", text="Set Origin", icon="MANIPUL")
+  
+        pie.operator("clip.set_axis", text="Set X Axis", icon="AXIS_FRONT").axis="X"
+        pie.operator("clip.set_axis", text="Set Y Axis", icon="AXIS_SIDE").axis="Y"
+
+
+
+class CLIP_timecontrol_pie(Menu):
+    # label is displayed at the center of the pie menu.
+    bl_label = "Time Control"
+    bl_idname = "clip.timecontrol_pie"
 
     def draw(self, context):
         layout = self.layout
 
         pie = layout.menu_pie()
-        pie.operator("clip.setup_tracking_scene", text="Setup Tracking Scene", icon="SCENE_DATA")
-        pie.operator("clip.solve_camera", text="Solve Camera", icon="CAMERA_DATA")
-        pie.operator("clip.set_plane", text="Set Floor", icon="MESH_PLANE")
-        pie.operator("clip.set_origin", text="Set Origin", icon="MANIPUL")
-        pie.operator("clip.set_solver_keyframe", text="Set Keyframe A", icon="KEY_HLT").keyframe='KEYFRAME_A'
-        pie.operator("clip.set_solver_keyframe", text="Set Keyframe B", icon="KEY_HLT").keyframe='KEYFRAME_B'
-        pie.operator("clip.set_axis", text="Set X Axis", icon="AXIS_FRONT").axis="X"
-        pie.operator("clip.set_axis", text="Set Y Axis", icon="AXIS_SIDE").axis="Y"
+      
+        pie.operator("screen.frame_jump", text="Jump to Startframe", icon="TRIA_LEFT").end=False
+        pie.operator("screen.frame_jump", text="Jump to Endframe", icon="TRIA_RIGHT").end=True
+      
+        pie.operator("clip.frame_jump", text="Start of Track", icon="REW").position="PATHSTART"
+        pie.operator("clip.frame_jump", text="End of Track", icon="FF").position="PATHEND"
+      
+        pie.operator("screen.animation_play", text="Playback Backwards", icon="PLAY_REVERSE").reverse=True
+        pie.operator("screen.animation_play", text="Playback Forwards", icon="PLAY").reverse=False
+      
+        pie.operator("screen.frame_offset", text="Previous Frame", icon="TRIA_LEFT").delta=-1
+        pie.operator("screen.frame_offset", text="Next Frame", icon="TRIA_RIGHT").delta=1
 
 
 
@@ -122,15 +225,13 @@ class CLIP_reconstruction_pie(Menu):
 ########## register ############
 
 
-
-
-
-
 def register():
     bpy.utils.register_class(CLIP_tracking_pie)
     bpy.utils.register_class(CLIP_marker_pie)
+    bpy.utils.register_class(CLIP_solver_pie)
     bpy.utils.register_class(CLIP_reconstruction_pie)
     bpy.utils.register_class(CLIP_clipsetup_pie)
+    bpy.utils.register_class(CLIP_timecontrol_pie)
 
 
     wm = bpy.context.window_manager
@@ -138,8 +239,11 @@ def register():
     kmi = km.keymap_items.new('wm.call_menu_pie', 'Q', 'PRESS').properties.name = "clip.marker_pie"
     kmi = km.keymap_items.new('wm.call_menu_pie', 'W', 'PRESS').properties.name = "clip.clipsetup_pie"
     kmi = km.keymap_items.new('wm.call_menu_pie', 'E', 'PRESS').properties.name = "clip.tracking_pie"
-    kmi = km.keymap_items.new('wm.call_menu_pie', 'S', 'PRESS', shift=True).properties.name = "clip.reconstruction_pie"
+    kmi = km.keymap_items.new('wm.call_menu_pie', 'S', 'PRESS', shift=True).properties.name = "clip.solver_pie"
+    kmi = km.keymap_items.new('wm.call_menu_pie', 'W', 'PRESS', shift=True).properties.name = "clip.reconstruction_pie"
 
+    km = wm.keyconfigs.addon.keymaps.new(name='Frames')
+    kmi = km.keymap_items.new('wm.call_menu_pie', 'A', 'PRESS', oskey=True).properties.name = "clip.timecontrol_pie"
 
 
 
@@ -148,7 +252,9 @@ def unregister():
     bpy.utils.unregister_class(CLIP_tracking_pie)
     bpy.utils.unregister_class(CLIP_marker_pie)
     bpy.utils.unregister_class(CLIP_clipsetup_pie)
+    bpy.utils.unregister_class(CLIP_solver_pie)
     bpy.utils.unregister_class(CLIP_reconstruction_pie)
+    bpy.utils.unregister_class(CLIP_timecontrol_pie)
 
 
 
