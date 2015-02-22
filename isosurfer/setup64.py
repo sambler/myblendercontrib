c03207d449c8c20736d2f3125711fecbb7d764f8
sambler <Develop@Shaneware.Biz>
2015-02-22 14:04:49 +1030

    add isosurfer files - iso surfer creates a mesh surface over particles the shared library needed to run this needs to be created using setup64.py

---
+++ isosurfer/setup64.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+from distutils.extension import Extension
+from Cython.Distutils import build_ext
+import Cython.Compiler.Options 
+
+Cython.Compiler.Options.annotate = True
+
+# only valid on windows
+#ext_modules = [Extension("mciso", ["mciso.pyx"],extra_compile_args=['/O2','/openmp','/fp:fast'])]
+# for *nix this should work
+ext_modules = [Extension("mciso", ["mciso.pyx"],extra_compile_args=['-O2','-fopenmp','-mfpmath=sse'])]
+#ext_modules = [Extension("cmolcore", ["cmolcore.pyx"],extra_compile_args=['/openmp'])]
+
+setup(
+  name = 'CubeSurfer core script',
+  cmdclass = {'build_ext': build_ext},
+  ext_modules = ext_modules
+)
